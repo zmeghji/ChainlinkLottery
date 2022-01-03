681adfb8390442332744c2fb586e803574d49d36
Zeeshan Meghji <zeeshan_meghji@hotmail.com>
2022-01-03 15:08:39 -0500

    retrieved price of eth from oracle

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,7 +1,49 @@
 pragma solidity ^0.6.6;
 
-contract Lottery{
-    constructor() public {
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol";
 
+contract Lottery{
+    using SafeMathChainlink for uint256;
+    AggregatorV3Interface internal ethUsdPriceFeed;
+
+    uint usdEntryFee;
+
+    constructor(address _ethUsdPriceFeed) public {
+        ethUsdPriceFeed = AggregatorV3Interface(_ethUsdPriceFeed);
+        usdEntryFee = 50;
     }
+
+    function enter() payable{
+        require(msg.value >= getEntranceFee());
+    }
+
+    function getEntranceFee public view returns(uint){
+        uint ethUsdPrice = getLatestEthUsdPrice();
+        // uint weiEntryFee = (usdEntryFee* 10**18) / (10**8 *ethUsdPrice);
+        uint weiEntryFee = (usdEntryFee* 10**10) / *ethUsdPrice;
+        return weiEntryFee;
+    }
+
+    function getLatestEthUsdPrice public view returns(uint){
+        (
+            uint80 roundID,
+            int price,
+            uint startedAt,
+            uint timeStamp,
+            uint80 answeredInRound
+        ) = ethUsdPriceFeed.latestRoundData();
+        return uint(price);
+    }
+
+    // function startLottery() public {
+
+    // }
+    // function stopLottery() public {
+
+    // }
+
+    // function pickWinner() public {
+
+    // }
 }
